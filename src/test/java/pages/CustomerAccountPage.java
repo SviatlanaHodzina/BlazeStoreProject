2bea9d2be41686be6da8875ff99298f8a7547a73
Sviatlana Hodzina <102725239+SviatlanaHodzina@users.noreply.github.com>
2022-09-13 15:37:25 +0300

    updated CustomerAccountPage.java

--- src/test/java/pages/CustomerAccountPage.java
+++ src/test/java/pages/CustomerAccountPage.java
@@ -1,5 +1,9 @@
 package pages;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -12,6 +16,7 @@ import java.time.Duration;
 
 import static org.openqa.selenium.By.xpath;
 import static pages.HomePage.*;
+import static pages.LogInModalWindowPage.LOG_IN_PAGE_BUTTON_LOG_IN_ELEMENT_XPATH;
 
 //This page is not implemented by developer yet
 //here should be implemented other methods when other features are developed for this page
@@ -20,8 +25,10 @@ public class CustomerAccountPage extends AbstractPage {
     // The following 2 commented Strings for the future implied to be developed features
     private final static String CUSTOMER_ACCOUNT_PAGE_CONTENT_ELEMENT_XPATH = "input here XPATH";
     private final static String CUSTOMER_ACCOUNT_PAGE_TITLE_ELEMENT_XPATH = "input here XPATH";
-    private final static String CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[@id='nameofuser']";
-    private final static String CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[@onclick='logOut()']";
+    final static String CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[@id,'nameofuser']";
+    final static String CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@id,'nameofuser') and contains (@style, 'display: block;')]";
+    final static String CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_NOT_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@id,'nameofuser') and contains (@style, 'display:none')]";
+    final static String CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[@onclick='logOut()']";
 
     public CustomerAccountPage() throws MalformedURLException {
         super();
@@ -37,28 +44,53 @@ public class CustomerAccountPage extends AbstractPage {
         return this;
     }
 
+    public HomePage logOut() throws MalformedURLException {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))).click();
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(LOG_IN_PAGE_BUTTON_LOG_IN_ELEMENT_XPATH)));
+        return new HomePage();
+    }
+
     public String getCustomerAccountPageTitle() {
         return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_TITLE_ELEMENT_XPATH))
                 .getAttribute("textContent");
     }
 
     public String getLinkNameInPlaceOfSignUpLink() {
-        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH)));
+        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH))
                 .getAttribute("textContent");
     }
 
+    public boolean usernameLinkInPlaceOfSignUpLinkIsDisplayed() {
+        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH))
+                .isDisplayed();
+    }
+
+    public boolean usernameLinkInPlaceOfSignUpLinkIsNotDisplayed() {
+        return driver.findElement(xpath(SIGN_UP_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH)).isDisplayed();
+    }
+
     public String getChangedColorOfLinkNameInPlaceOfSignUpLink() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH)));
         return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))
                 .getCssValue("color");
     }
 
     public String getLinkNameInPlaceOfLogInLink() {
-        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))
+        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_LOG_OUT_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))
                 .getAttribute("textContent");
     }
 
     public String getChangedColorOfLinkNameInPlaceOfLogInLink() {
-        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH))
+        return driver.findElement(xpath(CUSTOMER_ACCOUNT_PAGE_USERNAME_LINK_DISPLAYED_IN_NAVIGATING_MENU_ELEMENT_XPATH))
                 .getCssValue("color");
     }
 
