29261074474ab148b564959064935f24cb94ee01
Sviatlana Hodzina <102725239+SviatlanaHodzina@users.noreply.github.com>
2022-10-31 19:48:52 +0300

    updated HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,10 +7,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -18,28 +15,33 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import webdriver.WebDriverConnector;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.*;
 import java.util.List;
-import java.util.ResourceBundle;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.String.format;
 import static org.openqa.selenium.By.xpath;
-import static pages.LaptopsPage.getLaptopModelsListInResourceBundle;
-import static pages.PhonesPage.getPhoneModelsListInResourceBundle;
+import static pages.LaptopsPage.LAPTOP_ITEM_MODEL_LIST_ELEMENT_XPATH;
+import static pages.MonitorsPage.MONITOR_ITEM_MODEL_LIST_ELEMENT_XPATH;
+import static pages.PhonesPage.*;
 
 public class HomePage extends AbstractPage {
     public static final Logger logger = LogManager.getRootLogger();
 
     //methods for carousel-slides of items are not implemented yet
-    public final String BLAZE_PRODUCT_STORE_URL = "https://www.demoblaze.com/";
-    public final static String HOME_PAGE_NAVIGATING_MENU_ELEMENT_XPATH = "//*[@id='narvbarx']";
+    final static String BLAZE_PRODUCT_STORE_URL = "https://www.demoblaze.com/";
+    public final static String PATHNAME_PAGE = "/prod.html";
+    final static String HOME_PAGE_NAVIGATING_MENU_ELEMENT_XPATH = "//*[@id='narvbarx']";
     public final static String PRODUCT_CATEGORIES_CONTAINER_ON_HOME_PAGE_ELEMENT_XPATH = "//*[@id='contcont']";
     public final static String LOGO_STORE_TITLE_ON_HOME_PAGE_ELEMENT_XPATH = "//*[@id='nava']//text()";
     public final static String HOME_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@class,'nav-link') and contains(@href,'index.html')]";
@@ -51,15 +53,13 @@ public class HomePage extends AbstractPage {
     public final static String CART_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@class,'nav-link') and contains(@href,'cart.html')]";
     public final static String CART_PAGE_WRAPPER_ELEMENT_XPATH = "//*[@id='page-wrapper']";
     public final static String LOG_IN_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@class,'nav-link') and contains(@data-target,'logInModal')]";
-    public final static String LOG_IN_MODAL_POPPED_UP_WINDOW_ELEMENT_XPATH = "//*[@id='logInModal']//*[@class='modal-content']";
+    public final static String LOG_IN_MODAL_POPPED_UP_WINDOW_ELEMENT_XPATH = "//*[contains(@id,'logInModal') and contains(@style,'display: block;')]//*[@class='modal-content']";
     public final static String LOG_IN_PAGE_TITLE_ELEMENT_XPATH = "//*[@id='logInModalLabel']";
-    public final static String SIGN_UP_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@class,'nav-link') and contains(@data-target,'signInModal')]";
-    public final static String SIGN_UP_MODAL_POPPED_UP_WINDOW_ELEMENT_XPATH = "//*[@id='signInModal']//*[@class='modal-content']";
-    public final static String SIGN_UP_MODAL_WINDOW_TITLE_ELEMENT_XPATH = "//*[@id='signInModalLabel']";
+    final static String SIGN_UP_LINK_IN_NAVIGATING_MENU_ELEMENT_XPATH = "//a[contains(@class,'nav-link') and contains(@data-target,'signInModal')]";
+    public final static String SIGN_UP_MODAL_POPPED_UP_WINDOW_ELEMENT_XPATH = "//*[contains(@id,'signInModal') and contains(@class,'modal fade show')]//*[@class='modal-content']";
     public final static String PRODUCT_CONTAINER_ELEMENT_XPATH = "//div[@id='tbodyid']";
     public final static String PRODUCT_ITEM_IN_A_ROW_ELEMENT_XPATH = "//div[@id='tbodyid']/div";
     public final static String PRODUCT_ITEM_MODEL_ELEMENT_XPATH = "//div[@class='card-block']//a";
-    public final static String PHONE_ITEM_MODEL_LIST_ELEMENT_XPATH = "//div[@class='card-block']//a[contains(text(),'%s')]";
     public final static String PRODUCT_ITEM_PRICE_ELEMENT_XPATH = "//div[@class='card-block']//h5";
     public final static String PRODUCT_ITEM_DESCRIPTION_ELEMENT_XPATH = "//div[@class='card-block']//p";
     public final static String CATEGORIES_CONTAINER_LINK_0N_SIDE_MENU_ELEMENT_XPATH = "//*[@id='cat']";
@@ -149,15 +149,16 @@ public class HomePage extends AbstractPage {
         return new CartPage();
     }
 
-    public PhonesPage openPhonesLinkOnSideMenu(String phone) throws MalformedURLException {
-        driver.switchTo().activeElement();
+    public PhonesPage openPhonesLinkOnSideMenu(String phone, List<String> laptops, List<String> monitors) throws MalformedURLException {
+
         new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
                 .until(ExpectedConditions.elementToBeClickable
                         (xpath(format(PHONES_CATEGORY_LINK_0N_SIDE_MENU_ELEMENT_XPATH, phone)))).click();
         new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
-                .until(ExpectedConditions.attributeContains(xpath(PRODUCT_CONTAINER_ELEMENT_XPATH),
-                        "outerText", String.valueOf(getPhoneModelsListInResourceBundle())));
-        logger.info("You've been navigated to 'Contact' modal window");
+                .until((ExpectedConditions.invisibilityOfElementLocated(xpath(format(LAPTOP_ITEM_MODEL_LIST_ELEMENT_XPATH, laptops)))));
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until((ExpectedConditions.invisibilityOfElementLocated(xpath(format(MONITOR_ITEM_MODEL_LIST_ELEMENT_XPATH, monitors)))));
+        logger.info("You've been navigated to 'Phones' page");
         return new PhonesPage();
     }
 
@@ -228,5 +229,59 @@ public class HomePage extends AbstractPage {
                 }
             }
         }
+    }
+
+    public static List<String> getPhoneModelsListInResourceBundle() {
+        ResourceBundle resourceBundlePhones = ResourceBundle.getBundle("phones");
+        Enumeration<String> phoneKeys = resourceBundlePhones.getKeys();
+        List<String> phoneKeysList = new ArrayList<>();
+        while (phoneKeys.hasMoreElements()) {
+            String phoneKey = phoneKeys.nextElement();
+            String phoneValue = resourceBundlePhones.getString(phoneKey);
+            phoneKeysList.add(phoneValue);
+        }
+        return phoneKeysList;
+    }
+
+    public static List<String> getLaptopModelsListInResourceBundle() {
+        ResourceBundle resourceBundleLaptops = ResourceBundle.getBundle("laptops");
+        Enumeration<String> laptopKeys = resourceBundleLaptops.getKeys();
+        List<String> laptopKeysList = new ArrayList<>();
+        while (laptopKeys.hasMoreElements()) {
+            String laptopKey = laptopKeys.nextElement();
+            String laptopValue = resourceBundleLaptops.getString(laptopKey);
+            laptopKeysList.add(laptopValue);
+        }
+        return laptopKeysList.stream().sorted().collect(Collectors.toList());
+    }
+
+    public static List<String> getMonitorModelsListInResourceBundle() {
+        ResourceBundle resourceBundleMonitors = ResourceBundle.getBundle("monitors");
+        Enumeration<String> monitorKeys = resourceBundleMonitors.getKeys();
+        List<String> monitorKeysList = new ArrayList<>();
+        while (monitorKeys.hasMoreElements()) {
+            String monitorKey = monitorKeys.nextElement();
+            String monitorValue = resourceBundleMonitors.getString(monitorKey);
+            monitorKeysList.add(monitorValue);
+        }
+        return monitorKeysList.stream().sorted().collect(Collectors.toList());
+    }
+
+    public static Map<String, Integer> getPhoneModelsPriceMapInResourceBundle() {
+        ResourceBundle resourceBundlePhonePrices = ResourceBundle.getBundle("PhonesPriceListDollarUSA");
+        ResourceBundle resourceBundlePhones = ResourceBundle.getBundle("phones");
+        Enumeration<String> phonePriceKeys = resourceBundlePhonePrices.getKeys();
+        Enumeration<String> phoneKeys = resourceBundlePhonePrices.getKeys();
+        Map<String, Integer> phoneKeysMap = new HashMap<>();
+        while (phonePriceKeys.hasMoreElements()) {
+            while (phoneKeys.hasMoreElements()) {
+                String pricePhoneKey = phonePriceKeys.nextElement();
+                String modelPhoneKey = phoneKeys.nextElement();
+                int phonePriceValue = Integer.parseInt((resourceBundlePhonePrices.getString(pricePhoneKey)).replace("$", ""));
+                String phoneModelKey = resourceBundlePhones.getString(modelPhoneKey);
+                phoneKeysMap.put(phoneModelKey, phonePriceValue);
+            }
+        }
+        return phoneKeysMap;
     }
 }
