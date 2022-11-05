0f52d4b3faec87b441e526857e6aa629d87a20e1
Sviatlana Hodzina <102725239+SviatlanaHodzina@users.noreply.github.com>
2022-11-05 17:42:53 +0300

    CartPage updated

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -17,10 +17,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.time.Duration;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import static pages.HomePage.CART_PAGE_WRAPPER_ELEMENT_XPATH;
-import static pages.HomePage.saveScreenShot;
+import static pages.HomePage.*;
 
 public class CartPage extends AbstractPage {
     public final Logger logger = LogManager.getRootLogger();
@@ -29,7 +29,7 @@ public class CartPage extends AbstractPage {
     public final static String CART_PAGE_PRODUCT_ORDER_IMAGE_IN_PRODUCT_TABLE_ELEMENT_XPATH = "//*[@id='tbodyid']/tr/td[1]";
     public final static String CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH = "//*[@id='tbodyid']/tr/td[2]";
     public final static String CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH = "//*[@id='tbodyid']/tr/td[3]";
-    public final static String CART_PAGE_TOTAL_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH = "//*[@id='totalp']";
+    public final static String CART_PAGE_TOTAL_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH = "//h3[@id='totalp']";
     public final static String CART_PAGE_PRODUCT_ORDER_IN_PRODUCT_TABLE_DELETE_ELEMENT_XPATH = "//*[@id='tbodyid']/tr/td[4]/a";
     public final static String CART_PAGE_BUTTON_PLACE_ORDER_ELEMENT_XPATH = "//button[@data-target='#orderModal']";
     public final static String PLACE_ORDER_POPPED_UP_WINDOW_ELEMENT_XPATH = "//*[@id='orderModal']//*[@class='modal-content']";
@@ -50,38 +50,72 @@ public class CartPage extends AbstractPage {
         return this;
     }
 
+    @FindBy(how = How.XPATH, using = CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH)
+    public static List<WebElement> productOrderTitlesList;
+
     @FindBy(how = How.XPATH, using = CART_PAGE_PRODUCT_ORDER_ITEM_IN_PRODUCT_TABLE_ELEMENT_XPATH)
     private List<WebElement> productOrderItemsList;
 
     @FindBy(how = How.XPATH, using = CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH)
-    private List<WebElement> productOrdersPriceList;
+    private List<WebElement> productOrderPriceList;
+
+    @FindBy(how = How.XPATH, using = CART_PAGE_PRODUCT_ORDER_IMAGE_IN_PRODUCT_TABLE_ELEMENT_XPATH)
+    private List<WebElement> productOrderImagesList;
 
     public String getProductOrderItemTitle() {
-        return driver.findElement
-                        (By.xpath(CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        return driver.findElement(By.xpath(CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
                 .getAttribute("textContent");
     }
 
+    public List<String> getProductOrderTitlesListInACart() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS_LASTED))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        return productOrderTitlesList.stream()
+                .flatMap(productModel -> productModel.getAttribute("textContent").lines())
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getProductPriceListInACart() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS_LASTED))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        return productOrderPriceList.stream()
+                .flatMap(productPrice -> productPrice.getAttribute("textContent").lines())
+                .collect(Collectors.toList());
+    }
+
     public int getProductOrderItemPrice() {
-        return Integer.parseInt(driver.findElement
-                        (By.xpath(CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        return Integer.parseInt(driver.findElement(By.xpath(CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
                 .getAttribute("textContent"));
     }
 
-    public boolean productOrderItemHasImageInTheTable() {
-        return driver.findElement
-                        (By.xpath(CART_PAGE_PRODUCT_ORDER_IMAGE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
-                .isDisplayed();
+    public SortedMap<String, Integer> getPhoneModelsPriceListAddedInACartMap() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS_LASTED))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_TITLE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS_LASTED))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        SortedMap<String, Integer> phonePriceMap = new TreeMap<>(new HashMap<>());
+        Iterator<String> listIterator = getProductOrderTitlesListInACart().iterator();
+        Iterator<String> priceListIterator = getProductPriceListInACart().iterator();
+        while (listIterator.hasNext()) {
+            while (priceListIterator.hasNext()) {
+                String productModel = listIterator.next();
+                String productPrice = priceListIterator.next();
+                int productPriceValue = Integer.parseInt(productPrice);
+                phonePriceMap.put(productModel, productPriceValue);
+            }
+        }
+        return phonePriceMap;
     }
 
-    public int getSizeOfProductOrderList() {
-        return productOrderItemsList.size();
-    }
-
-    public int getTotalPriceOfProductOrders() {
-        return productOrdersPriceList.size();
-    }
-
+    /**
+     * this function is not implemented on site yet
+     * public String getDeleteConformationMessage(){
+     * return driver.findElement(By.xpath("")).getAttribute("textContent");
+     */
     public CartPage deleteAnOrder() {
         new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
                 .until(ExpectedConditions.elementToBeClickable
@@ -89,15 +123,15 @@ public class CartPage extends AbstractPage {
         logger.info("You have deleted the order.");
         return this;
     }
-//    this function is not implemented on site
-//    public String getDeleteConformationMessage(){
-//        return driver.findElement(By.xpath("")).getAttribute("textContent");
-//    }
 
-    public int getTotalPriceDisplayedInProductOrdersTable() {
-        return Integer.parseInt(driver.findElement
+    public int getTotalPriceDisplayedInACart() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS_LASTED))
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath(CART_PAGE_TOTAL_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        int actualTotalprice = Integer.parseInt(driver.findElement
                         (By.xpath(CART_PAGE_TOTAL_PRICE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
-                .getAttribute("textContent"));
+                .getAttribute("outerText"));
+        return !(actualTotalprice == 0) ? actualTotalprice : null;
     }
 
     public PlaceOrderModalWindow placeOrder() throws MalformedURLException {
@@ -108,6 +142,13 @@ public class CartPage extends AbstractPage {
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PLACE_ORDER_POPPED_UP_WINDOW_ELEMENT_XPATH)));
         logger.info("This is Place order form. Fill out the input fields, please.");
         return new PlaceOrderModalWindow();
+    }
+
+    public boolean productOrderItemHasImageInTheTable() {
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CART_PAGE_PRODUCT_ORDER_IMAGE_IN_PRODUCT_TABLE_ELEMENT_XPATH)));
+        return driver.findElement(By.xpath(CART_PAGE_PRODUCT_ORDER_IMAGE_IN_PRODUCT_TABLE_ELEMENT_XPATH))
+                .isDisplayed();
     }
 
     public static int getCountOfBrokenImages() throws IOException {
